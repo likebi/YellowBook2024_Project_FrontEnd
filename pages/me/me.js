08bda8117dd14b4707835e6b07d3b63827c8c076
Tan2455 <zhihongtan2455@gmail.com>
2024-07-19 10:36:35 +0800

    me（change)

--- pages/me/me.js
+++ pages/me/me.js
@@ -5,9 +5,9 @@ Page({
    */
   data: {
     userImage: '/static/me.png',
-    nickname: 'wechat_user',
     backgroundImage: "https://youimg1.c-ctrip.com/target/0101c1200061ynv4356C0_D_10000_1200.jpg?proc=autoorient",
-    Uid: "123456",
+    Uid: '',
+    nickname:'Wechat_user',
     follow_num: '0',
     fans_num: '0',
     like_num: '0',
@@ -15,7 +15,7 @@ Page({
     activeTagWidth: 64,
     activeTagLeft: 0,
     tabPositions: [22, 136, 250],
-    intro_user: '将用户的头像、昵称、用户 ID 和背景图存储到本地确保用户在不同页面的操作面可以获取到最新的信息。的作用是将当前用户的相关信息',
+    intro_user: '将用户的头',
 
     items: [
       {
@@ -87,7 +87,6 @@ Page({
     ],
   },
 
-
   onLoad: function () {
     this.getUserInfo();
     this.setData({
@@ -105,109 +104,60 @@ Page({
     wx.navigateBack();
   },
 
-  // onChooseBackground: function () {
-  //   wx.chooseImage({
-  //     count: 1,
-  //     sizeType: ['original', 'compressed'],
-  //     sourceType: ['album', 'camera'],
-  //     success: (res) => {
-  //       this.setData({
-  //         backgroundImage: res.tempFilePaths[0]
-  //       });
-  //       this.saveUserInfo();
-  //     },
-  //     fail: (err) => {
-  //       console.error(err);
-  //     }
-  //   });
-  // },
-
    // 获取用户头像
    onChooseAvatar(e) {
     console.log(e);
     this.setData({
-      userImage: e.detail.userImage
+      userImage: e.detail.avatarUrl
     })
     this.saveUserInfo();
   },
-  // 选择昵称
-  onChooseNickname(e) {
+
+   // 选择昵称
+   onChooseNickname(e) {
     console.log(e);
     this.setData({
       nickname: e.detail.value
     })
     this.saveUserInfo();
   },
-  // 存储用户信息
+
+  onChooseBackground: function () {
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        this.setData({
+          backgroundImage: res.tempFilePaths[0]
+        });
+        this.saveUserInfo();
+      },
+      fail: (err) => {
+        console.error(err);
+      }
+    });
+  },
+
+
   saveUserInfo() {
-    // userInfo
-    wx.setStorage({
-      key: 'userInfo',
-      data: {
-        userImage: this.data.userImage,
-        nickname: this.data.nickname
-      }
-    })
+    wx.setStorageSync('userInfo', {
+      userImage: this.data.userImage,
+      backgroundImage: this.data.backgroundImage,
+      nickname: this.data.nickname,
+      Uid: this.data.Uid,
+    });
   },
-  // 获取用户信息
+
   getUserInfo() {
-    console.log(this, '第一行');
-    let that = this;
-    wx.getStorage({
-      key: 'userInfo',
-      success: function(res) {
-        console.log(that, '函数内');
-        that.setData({
-          userImages: res.data.userImage,
-          nickname: res.data.nickname
-        })
-      }
-      // success: res => {
-      //   this.setData({
-      //     avatar_url: res.data.avatarUrl,
-      //     nickname: res.data.nickName
-      //   })
-      // }
-    })
+    const userInfo = wx.getStorageSync('userInfo') || {};
+    this.setData({
+      userImage: userInfo.userImage || this.data.userImage,
+      nickname: userInfo.nickname || this.data.nickname,
+      Uid: userInfo.Uid || this.data.Uid,
+      backgroundImage: userInfo.backgroundImage || this.data.backgroundImage,
+    });
   },
-  
-
-  // onChooseAvatar: function () {
-  //   wx.chooseImage({
-  //     count: 1,
-  //     sizeType: ['original', 'compressed'],
-  //     sourceType: ['album', 'camera'],
-  //     success: (res) => {
-  //       this.setData({
-  //         userImage: res.tempFilePaths[0]
-  //       });
-  //       this.saveUserInfo();
-  //       this.getUserInfo(); // 确保更新用户信息
-  //     },
-  //     fail: (err) => {
-  //       console.error(err);
-  //     }
-  //   });
-  // },
-
-  // saveUserInfo() {
-  //   wx.setStorageSync('userInfo', {
-  //     userImage: this.data.userImage,
-  //     backgroundImage: this.data.backgroundImage,
-  //     nickname: this.data.nickname,
-  //     Uid: this.data.Uid,
-  //   });
-  // },
-
-  // getUserInfo() {
-  //   const userInfo = wx.getStorageSync('userInfo') || {};
-  //   this.setData({
-  //     userImage: userInfo.userImage || this.data.userImage,
-  //     nickname: userInfo.nickname || this.data.nickname,
-  //     Uid: userInfo.Uid || this.data.Uid,
-  //     backgroundImage: userInfo.backgroundImage || this.data.backgroundImage,
-  //   });
-  // },
 
   switchTab(e) {
     const index = e.currentTarget.dataset.index;
@@ -229,14 +179,5 @@ Page({
     wx.navigateTo({
       url: '/pages/profile/clickProfile',
     });
-  },
-  like_post: function (e) {
-    const index = e.currentTarget.dataset.index;
-    const items = this.data.items;
-    items[index].isLiked = !items[index].isLiked;
-    items[index].loveImage = items[index].isLiked ? '../../static/love.png' : '../../static/love2.png';
-
-    this.setData({ items });
-  },
-
+  }
 })
