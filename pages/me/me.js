f16c7c9023b9db61ac84ff0145de7c27732b1720
Tan2455 <zhihongtan2455@gmail.com>
2024-07-19 09:33:43 +0800

    0719

--- pages/me/me.js
+++ pages/me/me.js
@@ -16,7 +16,77 @@ Page({
     activeTagLeft: 0,
     tabPositions: [22, 136, 250],
     intro_user: '将用户的头像、昵称、用户 ID 和背景图存储到本地确保用户在不同页面的操作面可以获取到最新的信息。的作用是将当前用户的相关信息',
+
+    items: [
+      {
+        id: 1,
+        itemImage: 'https://tse3-mm.cn.bing.net/th/id/OIP-C.1xIwmlF2qTdBieTBW7pkFAHaE8?rs=1&pid=ImgDetMain',
+        intro:'SOTO BETAWI 印度尼西亚黄椰汤',
+        text: "Agus",
+        loveImage: "../../static/love.png",
+        isLiked: false,
+      },
+      {
+        id: 2,
+        itemImage: 'https://video.cgtn.com/news/2022-09-29/Shanghai-s-Huangpu-River-boosts-economy-in-Yangtze-River-Delta-1dIM1NhfZKM/video/5f46a45b50c2455bbd175b1056c5122f/5f46a45b50c2455bbd175b1056c5122f.jpeg',
+        intro:'我的城市',
+        text: "真的的上海人",
+        loveImage: "../../static/love.png",
+        isLiked: false,
+      },
+
+      {
+        id: 3,
+        itemImage: 'https://tse1-mm.cn.bing.net/th/id/OIP-C.iACihbLRXkucP2AK7dhGfgHaEK?rs=1&pid=ImgDetMain',
+        intro:'北海道冬天温泉市',
+        text: "北海道666",
+        loveImage: "../../static/love.png",
+        isLiked: false,
+      },
+      {
+        id: 4,
+        itemImage: 'https://tse3-mm.cn.bing.net/th/id/OIP-C.jXEJdOp1OOMAMi3qB8FpPAHaE8?rs=1&pid=ImgDetMain',
+        intro:'马来西亚椰浆饭',
+        text: "马来西亚better then SG",
+        loveImage: "../../static/love.png",
+        isLiked: false,
+      },
+      {
+        id: 5,
+        itemImage: 'https://img14.360buyimg.com/mobilecms/s360x360_jfs/t1/100867/28/30156/133110/668275e8Fac838305/6f1ddab707bbfd08.jpg!q70.dpg.webp',
+        intro:'联想（Lenovo）拯救者27英寸2K FastIPS 原生180Hz',
+        text: "迷人的灰太狼",
+        loveImage: "../../static/love.png",
+        isLiked: false,
+      },
+
+      {
+        id: 3,
+        itemImage: 'https://tse1-mm.cn.bing.net/th/id/OIP-C.iACihbLRXkucP2AK7dhGfgHaEK?rs=1&pid=ImgDetMain',
+        intro:'北海道冬天温泉市',
+        text: "北海道666",
+        loveImage: "../../static/love.png",
+        isLiked: false,
+      },
+      {
+        id: 4,
+        itemImage: 'https://tse3-mm.cn.bing.net/th/id/OIP-C.jXEJdOp1OOMAMi3qB8FpPAHaE8?rs=1&pid=ImgDetMain',
+        intro:'马来西亚椰浆饭',
+        text: "马来西亚better then SG",
+        loveImage: "../../static/love.png",
+        isLiked: false,
+      },
+      {
+        id: 5,
+        itemImage: 'https://img14.360buyimg.com/mobilecms/s360x360_jfs/t1/100867/28/30156/133110/668275e8Fac838305/6f1ddab707bbfd08.jpg!q70.dpg.webp',
+        intro:'联想（Lenovo）拯救者27英寸2K FastIPS 原生180Hz',
+        text: "迷人的灰太狼",
+        loveImage: "../../static/love.png",
+        isLiked: false,
+      },
+    ],
   },
+
 
   onLoad: function () {
     this.getUserInfo();
@@ -109,5 +179,14 @@ Page({
     wx.navigateTo({
       url: '/pages/profile/clickProfile',
     });
-  }
+  },
+  like_post: function (e) {
+    const index = e.currentTarget.dataset.index;
+    const items = this.data.items;
+    items[index].isLiked = !items[index].isLiked;
+    items[index].loveImage = items[index].isLiked ? '../../static/love.png' : '../../static/love2.png';
+
+    this.setData({ items });
+  },
+
 })
