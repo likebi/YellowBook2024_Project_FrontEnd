94d87e5668ea08ee0a3ea459df75a1fa0c55e380
s_kai <songkai135790@live.com>
2024-07-22 15:50:36 +0800

    0722-分类显示

--- pages/Trip/trip.js
+++ pages/Trip/trip.js
@@ -1,70 +1,47 @@
 Page({
-
   /**
    * Page initial data
    */
   data: {
     isLiked: false,
-    items: [
-      
-      {
-        id: 1,
-        userImage:'https://ts1.cn.mm.bing.net/th/id/R-C.a86878663b4dbfad3a4722e53a755f79?rik=5m8UAKHAbQaOHw&riu=http%3a%2f%2fn.sinaimg.cn%2ftranslate%2f201%2fw600h401%2f20190306%2fh8cG-htwhfzs6942512.jpg&ehk=dQONT%2fYVOK9nlu6NYSlV5FsF5%2fNLxvEcFNua%2b8%2fv2M4%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
-        userimageComment:'https://tse4-mm.cn.bing.net/th/id/OIP-C.wsZ0tnm9gXJot1pfshIeEwHaE8?rs=1&pid=ImgDetMain',
-        text: "太爱了，上海迪斯尼乐园",
-        intro:'上海迪斯尼乐园',
-        loveImage: "../../static/love.png",
-        isLiked: false,
-      },
-      {
-        id: 2,
-        userImage:'https://ts1.cn.mm.bing.net/th/id/R-C.e26e0cd03aac4004f982c1d22fba5ed9?rik=Iu4vYHaoZZ9fgw&riu=http%3a%2f%2fwww.lvjie.com.cn%2fuploadfile%2f2021%2f1009%2f20211009113740528.jpg&ehk=ePgYh61ZshcX%2fE37EXmyeGPTvud%2bIyuznQYUrS9gA2o%3d&risl=&pid=ImgRaw&r=0',
-        userimageComment:'https://tse1-mm.cn.bing.net/th/id/OIP-C.6J6P38DlFPlnEWGElv4KAwHaLH?rs=1&pid=ImgDetMain',
-        text: "人人有爱，很想您",
-        intro:'九寨沟国家5A景区',
-        loveImage: "../../static/love.png",
-        isLiked: false,
-      },
-      {
-        id: 3,
-        userImage:'https://ts1.cn.mm.bing.net/th/id/R-C.a86878663b4dbfad3a4722e53a755f79?rik=5m8UAKHAbQaOHw&riu=http%3a%2f%2fn.sinaimg.cn%2ftranslate%2f201%2fw600h401%2f20190306%2fh8cG-htwhfzs6942512.jpg&ehk=dQONT%2fYVOK9nlu6NYSlV5FsF5%2fNLxvEcFNua%2b8%2fv2M4%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
-        userimageComment:'https://tse4-mm.cn.bing.net/th/id/OIP-C.wsZ0tnm9gXJot1pfshIeEwHaE8?rs=1&pid=ImgDetMain',
-        text: "太爱了，上海迪斯尼乐园",
-        intro:'上海迪斯尼乐园',
-        loveImage: "../../static/love.png",
-        isLiked: false,
-      },
-      {
-        id: 4,
-        userImage:'https://ts1.cn.mm.bing.net/th/id/R-C.a86878663b4dbfad3a4722e53a755f79?rik=5m8UAKHAbQaOHw&riu=http%3a%2f%2fn.sinaimg.cn%2ftranslate%2f201%2fw600h401%2f20190306%2fh8cG-htwhfzs6942512.jpg&ehk=dQONT%2fYVOK9nlu6NYSlV5FsF5%2fNLxvEcFNua%2b8%2fv2M4%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
-        userimageComment:'https://tse4-mm.cn.bing.net/th/id/OIP-C.wsZ0tnm9gXJot1pfshIeEwHaE8?rs=1&pid=ImgDetMain',
-        text: "太爱了，上海迪斯尼乐园",
-        intro:'上海迪斯尼乐园',
-        loveImage: "../../static/love.png",
-        isLiked: false,
-      },
-      {
-        id: 5,
-        userImage:'https://ts1.cn.mm.bing.net/th/id/R-C.a86878663b4dbfad3a4722e53a755f79?rik=5m8UAKHAbQaOHw&riu=http%3a%2f%2fn.sinaimg.cn%2ftranslate%2f201%2fw600h401%2f20190306%2fh8cG-htwhfzs6942512.jpg&ehk=dQONT%2fYVOK9nlu6NYSlV5FsF5%2fNLxvEcFNua%2b8%2fv2M4%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
-        userimageComment:'https://tse4-mm.cn.bing.net/th/id/OIP-C.wsZ0tnm9gXJot1pfshIeEwHaE8?rs=1&pid=ImgDetMain',
-        text: "太爱了，上海迪斯尼乐园",
-        intro:'上海迪斯尼乐园',
-        loveImage: "../../static/love.png",
-        isLiked: false,
-      },
-      {
-        id: 6,
-        userImage:'https://ts1.cn.mm.bing.net/th/id/R-C.a86878663b4dbfad3a4722e53a755f79?rik=5m8UAKHAbQaOHw&riu=http%3a%2f%2fn.sinaimg.cn%2ftranslate%2f201%2fw600h401%2f20190306%2fh8cG-htwhfzs6942512.jpg&ehk=dQONT%2fYVOK9nlu6NYSlV5FsF5%2fNLxvEcFNua%2b8%2fv2M4%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
-        userimageComment:'https://tse4-mm.cn.bing.net/th/id/OIP-C.wsZ0tnm9gXJot1pfshIeEwHaE8?rs=1&pid=ImgDetMain',
-        text: "太爱了，上海迪斯尼乐园",
-        intro:'上海迪斯尼乐园',
-        loveImage: "../../static/love.png",
-        isLiked: false,
-      },
-    
-    ],
+    items: [],
+  },
+  onLoad: function() {
+    this.getPost();
   },
 
+  getPost: function() {
+    const token = wx.getStorageSync('userToken'); // 从本地存储中获取 token
+    const tags = '旅游';
+    const encodedTags = encodeURIComponent(tags);
+    wx.request({
+      url: `http://localhost:3000/itemfilter?tags=${encodedTags}`, // 你的后端 API 地址
+      method: 'GET',
+      header: {
+        'Authorization': token
+      },
+      success: (res) => {
+        if (res.data.code === 200) {
+          // 将返回的数据设置到 page 的 items 数据中
+          this.setData({
+            items: res.data.data
+          });
+        } else {
+          console.error('获取数据失败:', res.data.msg);
+        }
+      },
+      fail: (err) => {
+        console.error('请求失败:', err);
+      }
+    });
+  },
+  navigateToContentPage: function(event) {
+    const id = event.currentTarget.dataset.id; 
+    console.log(`${id}`)
+    wx.navigateTo({
+      url: `/pages/contentpage/contentpage?id=${id}`,
+    });
+},
   like_post: function (e) {
     const index = e.currentTarget.dataset.index;
     const items = this.data.items;
