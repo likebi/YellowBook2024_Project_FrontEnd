34876d9be2e9b518106a825a4ec56872edc8b0cb
Tan2455 <zhihongtan2455@gmail.com>
2024-07-22 09:32:35 +0800

    笔记，主页跳转，内容

--- pages/follower/follower.js
+++ pages/follower/follower.js
@@ -1,16 +1,14 @@
 Page({
   data: {
 
-    arrowl: '<',
+
     value: '',
     followedList: [
       // 示例数据，可以根据需要替换
-      { id: 1, name: 'User 1', followerNumber: '100', background: '/static/me.png' },
-      { id: 2, name: 'User 2', followerNumber: '200', background: '/static/me.png' },
-      { id: 3, name: 'User 3', followerNumber: '100', background: '/static/me.png' },
-      { id: 4, name: 'User 4', followerNumber: '100', background: '/static/me.png' },
-      { id: 5, name: 'User 5', followerNumber: '100', background: '/static/me.png' },
-      { id: 6, name: 'User 6', followerNumber: '100', background: '/static/me.png' }
+      { id: 1, 
+        name: 'User 1', 
+        followerNumber: '100', 
+        background: '/static/me.png' },
     ],
   },
 
