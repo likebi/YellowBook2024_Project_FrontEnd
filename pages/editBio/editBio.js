8ea1dc6e5f8f7df61cb02d726d23e5f2e6e02daa
hongwen01 <12816272+hongwen01@user.noreply.gitee.com>
2024-07-26 11:37:49 +0800

    个人简介 用户界面 bio 0726

--- pages/editBio/editBio.js
+++ pages/editBio/editBio.js
@@ -5,28 +5,76 @@ Page({
     Bio: ''
   },
 
-
   onInput(event) {
     this.setData({
       currentLength: event.detail.value.length,
-      Bio: event.detail.value // 更新昵称
+      Bio: event.detail.value // 更新简介
     });
   },
 
   inputContent() {
     // 获取当前存储的用户信息
     const userInfo = wx.getStorageSync('userInfo') || {};
-    
-    // 更新昵称并存储到本地存储
+  
+    console.log('UserInfo:', userInfo); // 检查获取的用户信息
+  
+    // 更新简介并存储到本地存储
     userInfo.Bio = this.data.Bio;
     wx.setStorageSync('userInfo', userInfo);
-
-    wx.showToast({
-      title: '简介已保存',
-      icon: 'success',
-      duration: 2000
+  
+    // 获取 token 和其他信息
+    const token = wx.getStorageSync('userToken'); // 获取 token
+    const uid = userInfo.Uid || wx.getStorageSync('Uid'); // 确保获取到的 UID
+    const nickname = userInfo.nickname || wx.getStorageSync('nickname');
+    const userImage = userInfo.userImage || wx.getStorageSync('userImage');
+    const intro_user = userInfo.Bio; // 使用更新后的 Bio 作为简介
+  
+    console.log('Token:', token); // 检查 token
+    console.log('Uid:', uid); // 检查 uid
+    console.log('Nickname:', nickname); // 检查 nickname
+    console.log('UserImage:', userImage); // 检查 userImage
+    console.log('Bio:', intro_user); // 检查 Bio
+  
+    // 发送请求到服务器
+    wx.request({
+      url: 'http://localhost:3000/saveUserInfo', // 后端接口地址
+      method: 'POST',
+      data: {
+        nickname,
+        Uid: uid,
+        userImage,
+        intro_user
+      },
+      header: {
+        'Authorization': token,
+        'Content-Type': 'application/json' // 确保请求内容类型正确
+      },
+      success(res) {
+        if (res.data.code === 200) {
+          wx.showToast({
+            title: '简介已保存',
+            icon: 'success',
+            duration: 2000
+          });
+          console.log('服务器响应:', res.data);
+          wx.navigateBack(); // 返回上一页
+        } else {
+          wx.showToast({
+            title: '保存失败',
+            icon: 'none',
+            duration: 2000
+          });
+          console.error('服务器错误:', res.data.msg);
+        }
+      },
+      fail(err) {
+        wx.showToast({
+          title: '请求失败',
+          icon: 'none',
+          duration: 2000
+        });
+        console.error('请求失败:', err);
+      }
     });
-    console.log('提交的简介:', this.data.Bio);
-    wx.navigateBack(); // 返回上一页
-  },
+  }
 });
--- pages/userPage/userPage.js
+++ pages/userPage/userPage.js
@@ -13,24 +13,28 @@ Page({
     activeTagWidth: 64,
     activeTagLeft: 0,
     tabPositions: [22, 136, 250],
-    intro_user: '将用户的头像、昵称、用户 ID 和背最新的信息。的作用是将当前用户的相关信息',
+    intro_user: '',
+
+    item:[]
   },
 
   onLoad: function (options) {
-  console.log('Options received in onLoad:', options); // 调试信息
-  const ContentUid = options.ContentUid;
-  console.log('获得的uid:', ContentUid); // 调试信息
-  if (ContentUid) {
+    console.log('Options received in onLoad:', options);
+    const ContentUid = options.ContentUid;
+    console.log('获得的uid:', ContentUid);
+    if (ContentUid) {
+      this.setData({
+        ContentUid: ContentUid
+      });
+      this.sendUserInfo(ContentUid);
+      this.fetchUserData();
+    }
+    this.getUserInfo();
     this.setData({
-      ContentUid: ContentUid
+      activeTagLeft: this.data.tabPositions[this.data.currentTab]
     });
-  }
-  this.sendUserInfo(ContentUid)
-  this.getUserInfo();
-  this.setData({
-    activeTagLeft: this.data.tabPositions[this.data.currentTab]
-  });
-},
+  },
+  
 
   onShow() {
     this.getUserInfo();
@@ -50,7 +54,8 @@ Page({
         console.log('发送成功', res.data)
         this.setData({
           ContentUserAvatar: res.data.userAvatar,
-          ContentNickName: res.data.nickName
+          ContentNickName: res.data.nickName,
+          intro_user: res.data.intro_user
         })
       },
       fail: (err) => {  // 使用箭头函数
@@ -59,6 +64,38 @@ Page({
     })    
 },
 
+// 使用 wx.request 发送请求
+fetchUserData() {
+  const token = wx.getStorageSync('userToken'); // 从本地存储中获取 token
+  // let Uid = wx.getStorageSync('Uid');
+  let Uid = this.data.ContentUid; // 硬编码的 Uid，用于调试或测试
+  console.log(Uid);
+  if (!token) {
+    console.error('未找到授权 token');
+    return;
+  }
+  wx.request({
+    url: `http://localhost:3000/notes/notes/${Uid}`, // 你的后端 API 地址
+    method: 'GET',
+    header: {
+      'Authorization': token
+    },
+    success: (res) => {
+      if (res.data.code === 200) {
+        // 将返回的数据设置到 page 的 items 数据中
+        this.setData({
+          items: res.data.data
+        });
+      } else {
+        console.error('获取数据失败:', res.data.msg);
+      }
+    },
+    fail: (err) => {
+      console.error('请求失败:', err);
+    }
+  });
+},
+  
 
   back() {
     this.getUserInfo();
@@ -106,6 +143,19 @@ Page({
       activeTagLeft: this.data.tabPositions[current]
     });
   },
+
+  navigateToContentPage: function(event) {
+    const id = event.currentTarget.dataset.id; // 获取点击的发现内容的ID
+    if (id) {
+      console.log('点击');
+      wx.navigateTo({
+        url: `/pages/contentpage/contentpage?id=${id}` // 跳转到帖子页面，并传递帖子ID
+      });
+    } else {
+      console.warn('No ID found for clicked discover item.');
+    }
+  },
+
 
   edit_Profile() {
     wx.navigateTo({
